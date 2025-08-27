0e6b2d26d8e408aea4c236c2da65648c5de7189e
breaze <crack9810@gmail.com>
2025-08-27 18:50:27 -0500

    Comentando metodo sumar

--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -14,6 +14,10 @@ public class Manager {
         this.numeros.add(numero);
     }
 
+    /**
+     * Suma todos los números en la lista.
+     * @return La suma de los números.
+     */
     public int getSuma() {
         int total = 0;
         for (int numero : this.numeros) {
